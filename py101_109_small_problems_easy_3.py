16a52a272ee190c59fc8e13a3e119ff442578b8f
Konstantin Samoilov <konsamo@gmail.com>
2025-12-08 00:07:35 -0800

    Easy 3 second pass update

--- py101_109_small_problems_easy_3.py
+++ py101_109_small_problems_easy_3.py
@@ -109,10 +109,7 @@ def print_in_box(string, width=None):
         print('+' + '-' * (width + 2) + '+')
 
 # Further exploration 2 (I think I didn't do it the first time):
-# Previous code would only be able to handle 2 string lines, I think, so we gotta re-do
-
- 
-
+# (previous code would only be able to handle 2 string lines, I think, so we gotta re-do.)
 
 
 
@@ -145,5 +142,117 @@ def triangle(num):
     for el in range(1, num + 1):
         print(' ' * (num - el) + '*' * el)
     
+# Second pass:
+# Yeah, because you need an "i" iterator inside the loop, it's good to name your parameter something real like "height" for some clarity 
+def triangle(height):
+    for i in range(1, height + 1):
+        spaces = height - i
+        stars = i
+        print((' ' * spaces) + ('*' * stars))
+
+# 6
+
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+adverb = input("Enter an adverb: ")
+
+print(f"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!")
+print(f"The {adjective} {noun} {verb} {adverb} over the lazy {noun}.")
+print(f"The {noun} {adverb} {verb} up to Joe's {adjective} turtle.")
+
+# Skipping second pass of 6
+
+# 7 (I probably couldn't figure it out and retyped it)
+
+def is_double_num(num):
+    num_string = str(num)
+    center = len(num_string) // 2
+    left_number = num_string[:center]
+    right_number = num_string[center:]
+        
+    return left_number == right_number
+        
+def twice(num):   
+    if is_double_num(num):
+        return num
+    else:
+        return num * 2
+
+# Second pass: (still needed help but less so)
+
+def twice(num):
+    mid = len(str(num)) // 2
+    mid_idx = int(mid) # these two lines are kind of funny
+    
+    if len(str(num)) % 2 != 0:
+        return num * 2
+    elif str(num)[:mid_idx] != str(num)[mid_idx:]:
+        return num * 2
+    else:
+        return num
+    
+# 8
+
+def get_grade(g1, g2, g3):
+    score = (g1 + g2 + g3) / 3
+    
+    if 90 <= score <= 100:
+        return 'A'
+    elif 80 <= score < 90: 
+        return 'B'
+    elif 70 <= score < 80: 
+        return 'C'
+    elif 60 <= score < 70: 
+        return 'D'
+    else:
+        return 'F'
+    
+# Skipping second pass of 8
+
+# 9
+
+def clean_up(string):
+    result = ''
+    
+    for char in string:
+        if char.isalpha():
+            result += char
+        else:
+            if len(result) == 0:
+                result += ' '
+            elif result[-1] != ' ':
+                result += ' '
+                
+    return result
+
+
+print(clean_up("---what's my +*& line?") == " what s my line ")
+
+# Second pass (aside from doing ones like this in study sessions...)
+
+...
+
+# 10
+
+def century(year):
+    century_number = (year - 1) // 100 + 1
+    last_two = century_number % 100
+    
+    if last_two in (11, 12, 13):
+        suffix = 'th'
+    else:
+        last_digit = century_number % 10
+        if last_digit == 1:
+            suffix = 'st'
+        elif last_digit == 2:
+            suffix = 'nd'
+        elif last_digit == 3:
+            suffix = 'rd'
+        else:
+            suffix = 'th'
+            
+    return f"{century_number}{suffix}"
+
 # Second pass:
 
